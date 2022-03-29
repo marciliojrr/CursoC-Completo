37db7c0084982c1fe72befda64c690068b944b67
Marcílio Júnior <marciliojr@outlook.com>
2022-03-29 16:12:55 -0300

    Intro. HashSet e SortedSet

---
+++ Seção 15 - Generics, Set, Dictionary/HashSet_SortedSet1/HashSet_SortedSet1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSet_SortedSet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            foreach (string s in set) 
+            { 
+                Console.WriteLine(s); 
+            }
+        }
+    }
+}
