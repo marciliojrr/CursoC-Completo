0280f35fdd27e73c759b1699675cc1e6f8921d48
Marcílio Júnior <marciliojr@outlook.com>
2022-03-29 19:49:37 -0300

    Exercicio proposto - Dictionary

---
+++ Seção 15 - Generics, Set, Dictionary/ExercicioPropostoDict/ExercicioPropostoDict/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace ExercicioPropostoDict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Dictionary<string, int> dict = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] lines = sr.ReadLine().Split(',');
+                        string candidate = lines[0];
+                        int votes = int.Parse(lines[1]);
+
+                        if (dict.ContainsKey(candidate))
+                        {
+                            dict[candidate] += votes;
+                        }
+                        else
+                        {
+                            dict[candidate] = votes;
+                        }
+                    }
+
+                    foreach (KeyValuePair<string, int> line in dict)
+                    {
+                        Console.WriteLine($"{line.Key}: {line.Value}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
