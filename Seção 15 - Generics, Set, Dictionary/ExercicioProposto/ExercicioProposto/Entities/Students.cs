9bbed4f3abdcbf0527d6769c9bbc88701a28720d
Marcílio Júnior <marciliojr@outlook.com>
2022-03-29 18:24:14 -0300

    Exercicio proposto HashSet

---
+++ Seção 15 - Generics, Set, Dictionary/ExercicioProposto/ExercicioProposto/Entities/Students.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExercicioProposto.Entities
+{
+    internal class Students
+    {
+        public int RegistrationNumber { get; set; }
+
+        public override int GetHashCode()
+        {
+            return RegistrationNumber.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Students))
+            {
+                return false;
+            }
+            Students other = (Students)obj;
+
+            return RegistrationNumber.Equals(other.RegistrationNumber);
+        }
+    }
+}
---
+++ Seção 15 - Generics, Set, Dictionary/ExercicioProposto/ExercicioProposto/Program.cs
@@ -0,0 +1,49 @@
+﻿using ExercicioProposto.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ExercicioProposto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Course A
+            HashSet<Students> a = new HashSet<Students>();
+            Console.Write("How many students for course A? ");
+            int nA = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nA; i++)
+            {
+                a.Add(new Students { RegistrationNumber = int.Parse(Console.ReadLine()) });
+            }
+
+            // Course B
+            HashSet<Students> b = new HashSet<Students>();
+            Console.Write("How many students for course B? ");
+            int nB = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nB; i++)
+            {
+                b.Add(new Students { RegistrationNumber = int.Parse(Console.ReadLine()) });
+            }
+
+            // Course C
+            HashSet<Students> c = new HashSet<Students>();
+            Console.Write("How many students for course C? ");
+            int nC = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nC; i++)
+            {
+                c.Add(new Students { RegistrationNumber = int.Parse(Console.ReadLine()) });
+            }
+
+            HashSet<Students> students = new HashSet<Students>(a);
+            students.UnionWith(b);
+            students.UnionWith(c);
+
+            Console.Write($"Total students: {students.Count}");
+            
+        }
+    }
+}
