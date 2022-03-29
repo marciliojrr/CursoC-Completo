37fb6954e56e0460c1095d27da3b8955d1fc7fed
Marcílio Júnior <marciliojr@outlook.com>
2022-03-29 14:34:42 -0300

    Atualizando PrintService para uso com OBJECT - não recomendado

--- Seção 15 - Generics, Set, Dictionary/Generics1/Generics1/PrintService.cs
+++ Seção 15 - Generics, Set, Dictionary/Generics1/Generics1/PrintService.cs
@@ -4,10 +4,10 @@ namespace Generics1
 {
     internal class PrintService
     {
-        private int[] _values = new int[10];
+        private object[] _values = new object[10];
         private int _count = 0;
 
-        public void AddValue(int value)
+        public void AddValue(object value)
         {
             if (_count == 10)
             {
@@ -17,7 +17,7 @@ namespace Generics1
             _count++;
         }
 
-        public int First()
+        public object First()
         {
             if (_count == 0)
             {
--- Seção 15 - Generics, Set, Dictionary/Generics1/Generics1/Program.cs
+++ Seção 15 - Generics, Set, Dictionary/Generics1/Generics1/Program.cs
@@ -6,15 +6,20 @@ namespace Generics1
     {
         static void Main(string[] args)
         {
-            PrintServiceString printService = new PrintServiceString();
+            PrintService printService = new PrintService();
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
+                //int x = int.Parse(Console.ReadLine());
                 string x = Console.ReadLine();
                 printService.AddValue(x);
             }
+
+            int a = (int)printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
 
             printService.Print();
 
