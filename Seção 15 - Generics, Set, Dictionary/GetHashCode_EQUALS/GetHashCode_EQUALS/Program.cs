f3222c6000f325e5e28d545c6e1acb773b6e9fde
Marcílio Júnior <marciliojr@outlook.com>
2022-03-29 15:56:19 -0300

    GetHashCode e Equals

---
+++ Seção 15 - Generics, Set, Dictionary/GetHashCode_EQUALS/GetHashCode_EQUALS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GetHashCode_EQUALS.Entities;
+
+namespace GetHashCode_EQUALS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+
+            Console.WriteLine(a == b); // FALSO! Compara a referencia do ponteiro de memoria do objeto
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            
+        }
+    }
+}
