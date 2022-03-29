983934918adaf0bf9dcabdf6d99cb557b9363fb6
Marcílio Júnior <marciliojr@outlook.com>
2022-03-29 17:20:15 -0300

    Exercicio resolvido - HashSet

---
+++ Seção 15 - Generics, Set, Dictionary/ExercicioResolvido/ExercicioResolvido/Entities/LogRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExercicioResolvido.Entities
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            LogRecord other = (LogRecord)obj;
+
+            return Username.Equals(other.Username);
+        }
+    }
+}
