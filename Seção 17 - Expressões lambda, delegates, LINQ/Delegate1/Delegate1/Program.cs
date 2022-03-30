e9f53bc1c4ef10d2fc1a64d1c102b68e5997fff8
Marcílio Júnior <marciliojr@outlook.com>
2022-03-30 14:41:28 -0300

    Uso de delegates

---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Delegate1/Delegate1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Delegate1.Services;
+
+namespace Delegate1
+{
+    // Declarando um Delegate
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            double result = CalculationService.Sum(a, b);
+            Console.WriteLine(result);
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            double result1 = op(a, b);
+            Console.WriteLine(result1);
+        }
+    }
+}
