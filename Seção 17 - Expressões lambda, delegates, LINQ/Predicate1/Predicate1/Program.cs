9e99fa04a0054c4334751280157ed1ae020afbee
Marcílio Júnior <marciliojr@outlook.com>
2022-03-30 15:30:04 -0300

    Delegate: Action

--- Seção 17 - Expressões lambda, delegates, LINQ/Predicate1/Predicate1/Program.cs
+++ Seção 17 - Expressões lambda, delegates, LINQ/Predicate1/Predicate1/Program.cs
@@ -15,28 +15,42 @@ namespace Predicate1
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            /*
-            list.RemoveAll(p => p.Price >= 100.0);
+
+            // Modo 1
+            //list.foreach (updateprice) ;
+            //foreach (product p in list)
+            //{
+            //    console.writeline(p);
+            //}
+
+            // Modo 2
+            //Action<Product> act = UpdatePrice;
+            //list.ForEach(act);
+            //foreach (Product p in list)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            // Modo 3
+            //Action<Product> act = p => { p.Price += p.Price * 0.1; }; // tem que ter chaves, pois tem corpo mas retorna void
+            //list.ForEach(act);
+            //foreach (Product p in list)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            // Modo 4
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
-            */
-
-            // Ou
-
-            list.RemoveAll(ProductTest);
-            foreach (Product p in list)
-            {
-                Console.WriteLine(p);
-            }
-
         }
-        // PREDICATE
-        // Recebe um obj e retonar um bool
-        public static bool ProductTest(Product p)
+        // Funcao auxiliar
+        // Necessario para Modo 1 e Modo 2
+        static void UpdatePrice(Product p)
         {
-            return p.Price >= 100.0;
+            p.Price += p.Price * 0.1;
         }
     }
 }
