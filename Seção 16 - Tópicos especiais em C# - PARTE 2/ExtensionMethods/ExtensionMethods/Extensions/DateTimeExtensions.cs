5eeb10c30e9d29f0bf05053b92d71761b854f808
Marcílio Júnior <marciliojr@outlook.com>
2022-03-30 13:31:19 -0300

    Extension Methods - parte 1

---
+++ Seção 16 - Tópicos especiais em C# - PARTE 2/ExtensionMethods/ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExtensionMethods.Extensions // Podemos mudar o namespace para 'System', assim nao precisamos importar no programa principal
+{
+    internal static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return $"{duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture)} hours";
+            }
+            else
+            {
+                return $"{duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture)} days";
+            }
+        }
+    }
+}
---
+++ Seção 16 - Tópicos especiais em C# - PARTE 2/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExtensionMethods.Extensions;
+
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 03, 30, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
