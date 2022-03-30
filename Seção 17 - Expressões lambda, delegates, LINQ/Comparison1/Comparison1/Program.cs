f4cafaf4cf715334074de03919e612a66d9fd091
Marcílio Júnior <marciliojr@outlook.com>
2022-03-30 14:15:00 -0300

    Usando expressoes lambda

--- Seção 17 - Expressões lambda, delegates, LINQ/Comparison1/Comparison1/Program.cs
+++ Seção 17 - Expressões lambda, delegates, LINQ/Comparison1/Comparison1/Program.cs
@@ -14,6 +14,7 @@ namespace Comparison1
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
+            /*
             list.Sort(CompareProducts);
             foreach (Product p in list)
             {
@@ -29,12 +30,35 @@ namespace Comparison1
             {
                 Console.WriteLine(p);
             }
+            */
+
+            // Usando expressao lambda (funcao lambda = funcao anonima) em uma variavel
+            //                         (param1, param2 [sem tipo, pois serao inferidos]) => funcao a ser executada
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+            list.Sort(comp);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine("---");
+
+            // Usando expressao lambda direto na funcao Sort()
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
         }
 
         // DELEGATE - Referencia para funcao com Type Safety
+        // Necessario para rodar as linhas 17 e 26
+        /*
         static int CompareProducts(Product p1, Product p2)
         {
             return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
+        */
     }
 }
