e0bbdb9bdbfb20df59642f8e7cb0871443092616
Marcílio Júnior <marciliojr@outlook.com>
2022-03-30 13:56:14 -0300

    Comparsion - parte 1

---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Comparison1/Comparison1/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Comparison1.Entities
+{
+    internal class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Product other)
+        {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+    }
+}
---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Comparison1/Comparison1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Comparison1.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Comparison1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort();
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
