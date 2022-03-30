812e28a301240e0da1ba03e22490c164be6a00de
Marcílio Júnior <marciliojr@outlook.com>
2022-03-30 13:40:33 -0300

    Extension Methods - parte 2

--- Seção 16 - Tópicos especiais em C# - PARTE 2/ExtensionMethods/ExtensionMethods/Program.cs
+++ Seção 16 - Tópicos especiais em C# - PARTE 2/ExtensionMethods/ExtensionMethods/Program.cs
@@ -8,8 +8,16 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("DateTime Extensions");
+            Console.WriteLine("-------------------");
             DateTime dt = new DateTime(2022, 03, 30, 8, 10, 45);
             Console.WriteLine(dt.ElapsedTime());
+            Console.WriteLine();
+
+            Console.WriteLine("String Extensions");
+            Console.WriteLine("-----------------");
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
         }
     }
 }
