78961b801732638e4844f49bff329740dca48b7e
Marcílio Júnior <marciliojr@outlook.com>
2022-03-30 14:54:32 -0300

    Predicate 1

---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Predicate1/Predicate1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Predicate1.Entities;
+
+namespace Predicate1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            /*
+            list.RemoveAll(p => p.Price >= 100.0);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+            */
+
+            // Ou
+
+            list.RemoveAll(ProductTest);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+        // PREDICATE
+        // Recebe um obj e retonar um bool
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
